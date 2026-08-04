9ab148b290b8dfa6af8459a8c9061b19b896f820
philosphi <phin98@gmail.com>
2026-08-04 14:15:46 -0700

    feat: implement throttle

--- frontend-utilities/async-coordination/throttle/src/throttle.ts
+++ frontend-utilities/async-coordination/throttle/src/throttle.ts
@@ -2,6 +2,13 @@ export function throttle<T extends (...args: unknown[]) => unknown>(
   fn: T,
   interval: number,
 ): (...args: Parameters<T>) => void {
-  // your implementation here
-  return () => {};
+  let intervalId: ReturnType<typeof setTimeout>;
+  return function (this: unknown, ...args: Parameters<T>) {
+    if (!intervalId) {
+      fn.apply(this, args);
+      intervalId = setTimeout(() => {
+        clearTimeout(intervalId);
+      }, interval);
+    }
+  };
 }
--- frontend-utilities/async-coordination/throttle/tests/throttle.test.ts
+++ frontend-utilities/async-coordination/throttle/tests/throttle.test.ts
@@ -1,4 +1,36 @@
 import assert from "assert/strict";
 import { throttle } from "../src/throttle";
 
-// your tests here
+function wait(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+let timesCalled = 0;
+let base = 1;
+
+function multiply(this: { factor: number }) {
+  base *= this.factor;
+  timesCalled++;
+}
+
+const mult = {
+  factor: 2,
+  throttledMultiply: throttle(multiply, 100),
+};
+
+async function runTests() {
+  mult.throttledMultiply();
+  await wait(50);
+  mult.throttledMultiply();
+  assert.deepStrictEqual(timesCalled, 0);
+  assert.deepStrictEqual(base, 1);
+  await wait(50);
+  mult.throttledMultiply();
+  assert.deepStrictEqual(timesCalled, 1);
+  assert.deepStrictEqual(base, 2);
+  await wait(50);
+  assert.deepStrictEqual(timesCalled, 1);
+  assert.deepStrictEqual(base, 2);
+}
+
+runTests();
